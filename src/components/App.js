304812121efcc55ca3203033a645dea0c9d6ffd9
Your Name <you@example.com>
2021-02-14 18:01:26 +0600

    react hook

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Wikipedia from './Wikipedia'
+
+
+
+
+const App =()=>{
+    return<div><Wikipedia/></div>
+}
+export default App
---
+++ src/components/Wikipedia.js
@@ -0,0 +1,87 @@
+import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+
+
+
+const Wikipedia =()=>{
+    const[term,setTerm]=useState('')
+    const[results,setResults]=useState([])
+
+    
+
+   useEffect(()=>{
+       const search = async() =>{
+         const {data}=  await axios.get('https://en.wikipedia.org/w/api.php',{
+
+           params:{
+               action:'query',
+               list:'search',
+               origin:'*',
+               format:'json',
+               srsearch:term,
+
+           },
+
+           });
+           setResults(data.query.search)
+       };
+     if(term && !results.length){
+         search();
+
+     }else{
+         const timeoutId=setTimeout(()=>{
+             if(term){
+                 search()
+             }
+         },1000);
+         return()=>{
+             clearTimeout(timeoutId)
+         }
+     }
+
+       
+       
+   },[term])
+
+   const renderedResults=results.map((result)=>{
+       return(
+
+        <div key={results.pageid}className="item">
+            <div className="right floated content">
+                <a className="ui button"
+                href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                
+                >Go</a>
+            </div>
+            <div className="content">
+                <div className="header">
+                    {result.title}
+                </div>
+               <span dangerouslySetInnerHTML={{__html: result.snippet}}></span> 
+            </div>
+        </div>
+       );
+   })
+    return(
+    <div className="container">
+        <div className="ui form">
+            <div className="field">
+                <label>Enter Search term</label>
+                <input 
+                value={term}
+                onChange={(e)=> setTerm(e.target.value)}
+               
+
+                className="input"style={{color: "red"}} 
+                />
+
+            </div>
+        </div>
+        <div className="ui celled list">
+            {renderedResults}
+        </div>
+    </div>
+    
+    );
+}
+export default Wikipedia
